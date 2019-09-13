bda3d759071267aec20ad08481593dbdd83e58f2
Joachen D Busch <joachendbusch@gmail.com>
2019-09-13 14:32:02 -0700

    create hashtable

---
+++ src/main/java/hashtable/Node.java
@@ -0,0 +1,18 @@
+package hashtable;
+
+public class Node {
+
+    public String key;
+    public String value;
+    public Node next;
+
+    public void setNext(Node node) { this.next = node; }
+    public void setKey(String key) { this.key = key; }
+    public void setValue(String value) { this.value = value; }
+
+    public Node(String key, String value) {
+        setKey(key);
+        setValue(value);
+    }
+
+}
